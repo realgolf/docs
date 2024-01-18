84e02b7750d9f7ec300b416d03cf6ff7d4883a6b
Julian Hammer <jh@moinjulian.com>
2024-01-18 15:20:12 +0000

    Add al links to Footer

--- src/lib/components/Footer/PublicProfile.svelte
+++ src/lib/components/Footer/PublicProfile.svelte
@@ -4,4 +4,8 @@
 
 <div class="column">
 	<a href="/public-profile"><h3>Public Profile</h3></a>
+
+	<a href="/public-profile#username">Username</a>
+	<a href="/public-profile#name">Name</a>
+	<a href="/public-profile#bio">Bio</a>
 </div>
